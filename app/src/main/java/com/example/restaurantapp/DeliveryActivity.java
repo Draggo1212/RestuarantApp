5894acfd1a1f7c34c7689c096b0628916f4064f8
Draggo <careymcfadden123@gmail.com>
2022-10-25 12:05:26 -0400

    Added progress bar please help

--- app/src/main/java/com/example/restaurantapp/DeliveryActivity.java
+++ app/src/main/java/com/example/restaurantapp/DeliveryActivity.java
@@ -2,13 +2,29 @@ package com.example.restaurantapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class DeliveryActivity extends AppCompatActivity {
+
+    TextView deliveryTV;
+    TextView contactTV;
+    TextView locationTV;
+    TextView hoursTV;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_delivery);
+
+        deliveryTV = (TextView) findViewById(R.id.deliveryTV);
+        contactTV = (TextView) findViewById(R.id.contactTV);
+        locationTV = (TextView) findViewById(R.id.locationTV);
+        hoursTV = (TextView) findViewById(R.id.hoursTV);
+
+        Intent incomingIntent = getIntent();
+
     }
 }
--- app/src/main/java/com/example/restaurantapp/MainActivity.java
+++ app/src/main/java/com/example/restaurantapp/MainActivity.java
@@ -7,12 +7,20 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ProgressBar;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
     EditText searchET;
     Button menuBTN;
     Button deliveryBTN;
+    private ProgressBar mProgressBar;
+    private TextView mLoadingText;
+
+    private int mProgressStatus = 0;
+
+    private Handler mHandler = new Handler();
 
 
     @Override
@@ -24,9 +32,42 @@ public class MainActivity extends AppCompatActivity {
         menuBTN = (Button) findViewById(R.id.menuBTN);
         deliveryBTN = (Button) findViewById(R.id.deliveryBTN);
 
+        mProgressBar = (ProgressBar) findViewById(R.id.progressbar);
+        mLoadingText = (TextView) findViewById(R.id.LoadingCompleteTV);
+
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                while (mProgressStatus < 100)
+                    mProgressStatus++;
+                    android.os.SystemClock.sleep(50);
+                    mHandler.post(new Runnable() {
+                      @Override
+                      public void run() {
+                          mProgressBar.setProgress(mProgressStatus);
+                      }
+                    });
+                    mHandler.post(new Runnable(){
+                        @Override
+                        public void run() {
+                            mLoadingText.setVisibility(View.VISIBLE);
+                        }
+                    })
+                }
+        });
 
 
         menuBTN.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent myIntent = new Intent(MainActivity.this, MenuActivity.class);
+
+                startActivity(myIntent);
+            }
+        });
+
+
+        deliveryBTN.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Intent myIntent = new Intent(MainActivity.this, MenuActivity.class);
